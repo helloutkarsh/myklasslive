5d7ddd9967462739a3baec8ad57573f628cdd342
greynited <utkarshbajpai44@gmail.com>
2021-01-25 21:08:21 +0530

    gh page first ever update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
     return (
         <>
               <div className="heading"><h2>About</h2></div>
-                <h2>GH Pages</h2>
+                <h2>GH Pages 2</h2>
 
               <div className="content">
                   An effort to provide better and more reliable 'single source of truth' platform to college students for exposure to better opportunities in the immediate future
