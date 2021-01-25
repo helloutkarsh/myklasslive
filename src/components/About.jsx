e63d22202abd9b49f31a037fac032eda4cf79e94
greynited <utkarshbajpai44@gmail.com>
2021-01-25 20:37:55 +0530

    about 2nd change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,10 +12,11 @@ function About() {
     return (
         <>
               <div className="heading"><h2>About</h2></div>
-
+                <h2>GH Pages</h2>
 
               <div className="content">
                   An effort to provide better and more reliable 'single source of truth' platform to college students for exposure to better opportunities in the immediate future
+                  
               </div>
 
 
