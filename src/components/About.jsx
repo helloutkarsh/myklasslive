0972fb7b54d61651e0b88809eff18c113bd174ee
greynited <utkarshbajpai44@gmail.com>
2021-01-25 20:14:37 +0530

    about badal diya

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ function About() {
 
 
               <div className="content">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur suscipit dolore ex ducimus exercitationem corporis non asperiores? Nam, cum! Laudantium sint vitae beatae nihil voluptatibus necessitatibus placeat nisi! Nulla, optio praesentium reiciendis voluptatibus, assumenda pariatur fuga quidem vero dignissimos iste odio! Atque quisquam accusamus tota Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti quibusdam doloribus autem molestiae a hic quam cupiditate nihil aliquam possimus, at iure aspernatur. At reprehenderit nisi distinctio est minus quidem velit sit voluptate! Blanditiis ex voluptates nulla voluptatum vero non laudantium. Ipsum ipsa amet nemo voluptas vero debitis harum maxime provident nihil doloribus ducimus facilis, ab eum exercitationem libero ut! Veritatis quos vero, fuga cum eveniet deserunt minus quam enim autem accusamus ullam ducimus.
+                  An effort to provide better and more reliable 'single source of truth' platform to college students for exposure to better opportunities in the immediate future
               </div>
 
 
